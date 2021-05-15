0dac4b303e403f5d89be0f19c50cc20c6567a275
pakjkwan <pakjkwan@gmail.com>
2021-05-15 11:06:08 +0900

    add code

--- py_mul_inher/main.py
+++
@@ -1,6 +0,0 @@
-from _12_multi_inher import *
-if __name__=='__main__':
-    d = D()
-    d.dothis()
-    print(D.mro())
-
--- tf_basic/multiply.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-a = tf.placeholder("float")
-b = tf.placeholder("float")
-y = tf.multiply(a,b)
-sess = tf.Session()
-print(sess.run(y, feed_dict={a:3, b:3}))
